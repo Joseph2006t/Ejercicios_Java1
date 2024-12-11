2dc596456466cb687ac7a9a2241a4895b5fb7079
Joseph2006t <laizatellojoseph@gmail.com>
2024-12-11 11:18:50 -0500

    primer commit

---
+++ src/main/java/com/mycompany/ejercicios_java1/Ejercicios_Java1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicios_java1;
+
+/**
+ *
+ * @author JOSEPH
+ */
+public class Ejercicios_Java1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
